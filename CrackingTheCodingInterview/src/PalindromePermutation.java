32e8993f9ea960b7f5c30acacd9a1ef122f8a4cd
Yashwanth <polavarapuyashwanth@gmail.com>
2023-05-24 13:37:19 +0530

    Added PalindromePermutation to Cracking the Coding interview folder

---
+++ CrackingTheCodingInterview/src/PalindromePermutation.java
@@ -0,0 +1,14 @@
+// Given a string write a function to check if it is a permutation of a palindrome.
+// Ex :
+// Input : Tact Coa
+// Output : True
+// Explanation : As all the letter frequencies of String "TactCoa" obey palindrome rules
+//               t - 2 , c - 2, a - 2, o - 1
+//               So the potential palindrome string can be tcaoact and given string can be permutation of this
+
+
+public class PalindromePermutation {
+    public static void main(String[] args) {
+
+    }
+}
